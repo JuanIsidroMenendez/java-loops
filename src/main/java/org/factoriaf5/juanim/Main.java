bd708a77e419a2c09e6748ca6e78a338bce5bba3
Juan <juan.isidro.menendez@gmail.com>
2026-07-22 10:41:54 +0200

    feat: multiple minor updates

--- src/main/java/org/factoriaf5/juanim/Main.java
+++ src/main/java/org/factoriaf5/juanim/Main.java
@@ -1,5 +1,13 @@
 package org.factoriaf5.juanim;
 
+import java.util.List;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        MultiplicationTable table = new MultiplicationTable();
+        TablePrinter printer = new TablePrinter();
+
+        List<String> lines = table.generate(5);
+        printer.print(lines);
+    }
 }
