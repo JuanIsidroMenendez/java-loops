c9c36ff038e7c40e532a2919170b4d905e5393bc
Juan <juan.isidro.menendez@gmail.com>
2026-07-22 12:12:40 +0200

    test: add NegativeNumber test

--- src/test/java/org/factoriaf5/juanim/MultiplicationTableTest.java
+++ src/test/java/org/factoriaf5/juanim/MultiplicationTableTest.java
@@ -1,6 +1,5 @@
 package org.factoriaf5.juanim;
 
-
 import org.junit.jupiter.api.Test;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -17,7 +16,7 @@ class MultiplicationTableTest {
         assertEquals("5 x 1 = 5", result.get(0));
         assertEquals("5 x 10 = 50", result.get(9));
     }
-    @Test
+    @Test //Test de escenario. Comprueba el funcionamiento del método generate con 0.
     void generatesTableForZero() {
         MultiplicationTable table = new MultiplicationTable();
         List<String> result = table.generate(0);
@@ -25,5 +24,12 @@ class MultiplicationTableTest {
         assertEquals("0 x 1 = 0", result.get(0));
         assertEquals("0 x 10 = 0", result.get(9));
     }
-    
+    @Test //Test de escenario. Comprueba el funcionamiento de los números negativos.
+    void generatesTableForNegativeNumber() {
+        MultiplicationTable table = new MultiplicationTable();
+        List<String> result = table.generate (-3);
+
+        assertEquals("-3 x 1 = -3", result.get(0));
+        assertEquals("-3 x 10 = -30", result.get(9));
+    }
 }
