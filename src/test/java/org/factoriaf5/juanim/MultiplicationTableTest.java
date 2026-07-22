9aa917a7674a4188d058a77190fb170111923ad9
Juan <juan.isidro.menendez@gmail.com>
2026-07-22 10:41:01 +0200

    feat: create MultiplicationTableTest

--- src/test/java/org/factoriaf5/juanim/MultiplicationTableTest.java
+++ src/test/java/org/factoriaf5/juanim/MultiplicationTableTest.java
@@ -1,5 +1,19 @@
 package org.factoriaf5.juanim;
 
-public class MultiplicationTableTest {
-    
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MultiplicationTableTest {
+
+    @Test
+    void generatesTableForFive() {
+        MultiplicationTable table = new MultiplicationTable();
+
+        List<String> result = table.generate(5);
+
+        assertEquals(10, result.size());
+        assertEquals("5 x 1 = 5", result.get(0));
+        assertEquals("5 x 10 = 50", result.get(9));
+    }
 }
